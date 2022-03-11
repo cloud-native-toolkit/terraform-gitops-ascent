c41a81de65680a2f50df8142f7707ad082c1f03c
Noé Samaille <noe.samaille@ibm.com>
2022-03-11 16:20:37 +0100

    Updated token generation
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- token.sh
+++ token.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-TOKEN=$(echo $(($RANDOM*$RANDOM*$RANDOM)) | base64 | sed 's/=//g' | head -c 20; echo) && echo "{ \"token\": \"${TOKEN}\" }"
+echo "{ \"token\": \"${RANDOM}${RANDOM}${RANDOM}\" }"
